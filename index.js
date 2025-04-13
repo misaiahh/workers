8c7d4079d50511e68b24a5d19a50b47197d42ee7
Micky Hallabrin <misaiahh@proton.me>
2025-04-13 12:53:29 -0400

    working example

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 import { Worker } from 'worker_threads';
 
+function runService(workerData) {
+    return new Promise((resolve, reject) => {
+        const input = {};
+        const worker = new Worker('./threads/worker.js', { workerData });
+        worker.on('message', resolve);
+        worker.on('error', reject);
+        worker.on('exit', (code) => {
+            if (code !== 0)
+                reject(new Error(`Worker stopped with exit code ${code}`));
+        })
+    });
+}
+
+async function run() {
+    const result = await runService('world')
+    console.log(result);
+}
+
+run().catch(err => console.error(err))
